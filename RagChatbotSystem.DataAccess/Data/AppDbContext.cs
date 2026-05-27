774ded4eb087cb6856cca25e2a3891fbe148c0ed
minhvuong09090808@gmail.com <minhvuong09090808@gmail.com>
2026-05-27 08:55:25 +0700

    first commit

---
+++ RagChatbotSystem.DataAccess/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RagChatbotSystem.DataAccess.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // DbSet
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
