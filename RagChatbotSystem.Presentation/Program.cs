0cc1e7d56928752178fe292dce5b6b155cd62905
Vo Tan Thinh <votanthinhcri28@gmail.com>
2026-05-29 20:27:47 +0700

    Add auto EF migration startup to Program.cs

--- RagChatbotSystem.Presentation/Program.cs
+++ RagChatbotSystem.Presentation/Program.cs
@@ -122,6 +122,13 @@ namespace RagChatbotSystem.Presentation
 
             var app = builder.Build();
 
+            if (app.Environment.IsDevelopment())
+            {
+                using var scope = app.Services.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.Migrate();
+            }
+
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
